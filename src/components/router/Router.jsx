24a944444aa22ec440d018757bd09128dfa691c6
wadadones <jones3429@outlook.jp>
2021-10-19 08:35:44 +0900

    add atom components

--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
 import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { Header } from "../atoms/layout/Header";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
+          <Header />
           Home
           <PrimaryButton>Primary Button</PrimaryButton>
           <SecondaryButton>Secondary Button</SecondaryButton>
