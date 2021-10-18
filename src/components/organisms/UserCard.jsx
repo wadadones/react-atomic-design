07098522b1f2c3df755a07b6d31b15d0e8789dc8
wadadones <jones3429@outlook.jp>
2021-10-19 08:39:47 +0900

    add organisms components

---
+++ src/components/organisms/UserCard.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { Card } from "../../atoms/card/Card";
+import { UserIconWithName } from "../../molecules/user/UserIconWithName";
+
+export const UserCard = (props) => {
+  const { user } = props;
+  return (
+    <Card>
+      <UserIconWithName name={user.name} image={user.image} />
+      <SDl>
+        <dt>mail</dt>
+        <dd>{user.email}</dd>
+        <dt>phone</dt>
+        <dd>{user.phone}</dd>
+        <dt>company</dt>
+        <dd>{user.company.name}</dd>
+        <dt>web</dt>
+        <dd>{user.website}</dd>
+      </SDl>
+    </Card>
+  );
+};
+
+const SDl = styled.dl`
+  text-align: left;
+  margin-bottom: 0;
+  dt {
+    float: left;
+  }
+  dd {
+    padding-left: 40px;
+    padding-bottom: 8px;
+    overflow-wrap: break-word;
+  }
+`;
