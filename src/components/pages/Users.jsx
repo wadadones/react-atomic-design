2414b3bd15f3fad2ffdbe5c38e3fa95d66dfcb2f
wadadones <jones3429@outlook.jp>
2021-10-22 09:31:37 +0900

    add admin switch button

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,4 +1,7 @@
+import { useContext } from "react";
 import styled from "styled-components";
+import { UserContext } from "../../providers/UserProvider";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
 
@@ -17,10 +20,16 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
+  const { userInfo, setUserInfo } = useContext(UserContext);
+  const onClickSwitch = () => {
+    setUserInfo({ isAdmin: !userInfo.isAdmin });
+  }
   return (
     <SContainer>
       <h2>Users Page</h2>
       <SearchInput />
+      <br/>
+      <SecondaryButton onClick={onClickSwitch} >切り替え</SecondaryButton>
       <SUserArea>
         {users.map((user) => (
           <UserCard key={user.id} user={user} />
